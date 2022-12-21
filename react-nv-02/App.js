35affd0a13e4b9d4807e029abf0311d803cf4352
AnastasiiaKotielnikova <akotelnikova1993@gmail.com>
2022-12-21 17:33:32 +0200

    add bg

--- react-nv-02/App.js
+++ react-nv-02/App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, ImageBackground } from "react-native";
 import { RegistrationScreen } from "./Screens/RegistrationScreen/RegistrationScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <ImageBackground
+        style={styles.image}
+        source={require("./images/Photo_BG.jpg")}
+      ></ImageBackground>
       <RegistrationScreen />
       <StatusBar style="auto" />
     </View>
@@ -15,6 +19,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
     alignItems: "center",
     justifyContent: "center",
   },
